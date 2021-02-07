d45fab0205ce5107d44d52a3748a8861e3a6045f
Adam Pacheco <adampacheco98@hotmail.com>
2021-02-07 03:14:03 -0500

    Collision and Movement added
    Collision for the ground, player, floating platforms, and log bridges were added alongside rigid bodies. Code for player movement and jumping has also been added.

---
+++ Assignment 1/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump")) {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+        }
+        
+    }
+}
